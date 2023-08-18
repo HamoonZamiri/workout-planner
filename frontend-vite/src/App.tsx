c769ccf342dbfa474039d55cbfa52ff99e93d14e
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2023-08-18 18:19:01 -0400

    feat: bootstrapped vite app with react-ts template

---
+++ frontend-vite/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom"
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+        
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
