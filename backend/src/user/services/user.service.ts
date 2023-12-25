b27edbb228f88bfcfebe5b823a5acd971ba3ceb0
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2023-12-25 14:34:38 -0500

    feat: add basic unit tests

--- backend/src/user/services/user.service.ts
+++ backend/src/user/services/user.service.ts
@@ -8,60 +8,84 @@ import { User } from "../entities/user.entity";
 const UserRepository = FitlogCoreDataSource.getRepository(User);
 
 const createToken = (_id: string) => {
-    const token = jwt.sign({ _id }, process.env.SECRET || "");
-    return token;
-}
-
-const post = async(email: string, password: string) => {
-    if(!email || !password){
-        throw new Error("Email and Password are required");
-    }
-    if(!validator.isEmail(email)){
-        throw new Error("Email is invalid");
-    }
-
-    // strong password is at least 8 characters long and contains at least one lowercase letter, one uppercase letter, one number, and one symbol
-    if(!validator.isStrongPassword(password)){
-        throw new Error("Password is not strong enough");
-    }
-
-    const exists = await UserRepository.findOneBy({email});
-    if(exists) {
-        throw new Error("Email already exists");
-    }
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const user = await UserRepository.create({ email, password: hashedPassword });
-    return UserRepository.save(user);
+	const token = jwt.sign({ _id }, process.env.SECRET || "");
+	return token;
 };
 
-const login = async(email: string, password: string) => {
-    if(!email || !password){
-        throw new Error("Email and Password are required");
-    }
-    const user = await UserRepository.findOneBy({ email });
-    if(!user){
-        throw new Error(`No user found with email ${email}`);
-    }
-    const comparePass = await bcrypt.compare(password, user.password);
-    if(!comparePass){
-        throw new Error("Password is incorrect");
-    }
-    return user;
+const isValidEmail = (email: string | null): boolean => {
+	if (!email) {
+		return false;
+	}
+	if (!validator.isEmail(email)) {
+		return false;
+	}
+	return true;
 };
 
-const getById = async(id: string) => {
-const user = await UserRepository.findOneBy({id})
-    if(!user){
-        throw new Error("User not found");
-    }
-    return user;
-}
+const isValidPassword = (password: string | null): boolean => {
+	if (!password) {
+		return false;
+	}
+	if (!validator.isStrongPassword(password)) {
+		return false;
+	}
+	return true;
+};
+
+const post = async (email: string, password: string) => {
+	if (!isValidEmail(email)) {
+		throw new Error("Email is not valid");
+	}
+
+	if (!isValidPassword(password)) {
+		throw new Error(
+			"Password is not valid, passwords must be at least 8 characters long" +
+				" and contain at least 1 lowercase, 1 uppercase, 1 number, and 1 symbol"
+		);
+	}
+
+	const exists = await UserRepository.findOneBy({ email });
+	if (exists) {
+		throw new Error("Email already exists");
+	}
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+	const user = await UserRepository.create({
+		email,
+		password: hashedPassword,
+	});
+	return UserRepository.save(user);
+};
+
+const login = async (email: string, password: string) => {
+	if (!email || !password) {
+		throw new Error("Email and Password are required");
+	}
+	const user = await UserRepository.findOneBy({ email });
+	if (!user) {
+		throw new Error(`No user found with email ${email}`);
+	}
+	const comparePass = await bcrypt.compare(password, user.password);
+	if (!comparePass) {
+		throw new Error("Password is incorrect");
+	}
+	return user;
+};
+
+const getById = async (id: string) => {
+	const user = await UserRepository.findOneBy({ id });
+	if (!user) {
+		throw new Error("User not found");
+	}
+	return user;
+};
 
 const UserService = {
-    createToken,
-    post,
-    login,
-    getById,
-}
+    isValidEmail,
+    isValidPassword,
+	createToken,
+	post,
+	login,
+	getById,
+};
 export default UserService;
