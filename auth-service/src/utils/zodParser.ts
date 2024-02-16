0d0112f1887d63b18837255f47570485bce7894f
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-02-15 21:32:05 -0500

    chore: return better error message

--- auth-service/src/utils/zodParser.ts
+++ auth-service/src/utils/zodParser.ts
@@ -8,10 +8,7 @@ export async function zodParse<T extends AnyZodObject>(
 ): Promise<z.infer<T>> {
   const parsedRequest = await schema.safeParseAsync(req);
   if (!parsedRequest.success) {
-    throw new AppError(
-      400,
-      parsedRequest.error.issues[0]?.message || "Invalid request body",
-    );
+    throw new AppError(400, "Missing required field in request");
   }
   return parsedRequest.data;
 }
