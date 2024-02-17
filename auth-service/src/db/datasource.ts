7aa7035c2a87ef8eebdebf8dc4a6b46f5ddb79da
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-02-17 13:19:26 -0500

    fix: replace constants with env variables for gitguardian

--- auth-service/src/db/datasource.ts
+++ auth-service/src/db/datasource.ts
@@ -1,14 +1,15 @@
 import { DataSource } from "typeorm";
-import { AuthUser } from "../entities/authUser.entity";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const AuthDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: 5432,
-  username: "postgres",
-  password: "postgres",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   database:
-    process.env.NODE_ENV === "test" ? "test_auth_service" : "auth_service",
+    process.env.NODE_ENV === "test" ? "test_auth_service" : process.env.DB_NAME,
   synchronize: true,
   entities: [__dirname + "/../**/*.entity.{js,ts}"],
 });
