c7a7a029bb0a3efa31252d1ce3efbb12825f4a53
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-02-19 17:24:26 -0500

    chore: use a better error message returned by the zoderror

--- backend/src/utils/zod.ts
+++ backend/src/utils/zod.ts
@@ -3,12 +3,13 @@ import { z, AnyZodObject } from "zod";
 import AppError from "./AppError";
 
 export async function zParse<T extends AnyZodObject>(
-	schema: T,
-	req: Request
+  schema: T,
+  req: Request,
 ): Promise<z.infer<T>> {
-	const parsedRequest = await schema.safeParseAsync(req);
-	if (!parsedRequest.success) {
-		throw new AppError(400, parsedRequest.error.issues[0].message);
-	}
-	return parsedRequest.data;
+  const parsedRequest = await schema.safeParseAsync(req);
+  if (!parsedRequest.success) {
+    const firstIssue = parsedRequest.error.issues[0];
+    throw new AppError(400, firstIssue.message + " " + firstIssue.path);
+  }
+  return parsedRequest.data;
 }
