0f19547b5de95c512cfa90e4624b566f4b5455ff
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-02-22 11:03:47 -0500

    feat: add reducer action REFRESH to refresh JWT tokens

--- frontend-vite/src/context/AuthContext.tsx
+++ frontend-vite/src/context/AuthContext.tsx
@@ -1,45 +1,62 @@
 import { createContext, useEffect, useReducer } from "react";
 import { User } from "../utils/types";
+import { useCookies } from "react-cookie";
 
 type AuthContextType = {
-	user: User | null;
+  user: User | null;
 };
 
 type ACTIONTYPE =
-	| { type: "LOGIN"; payload: User }
-	| { type: "LOGOUT"; payload: null };
+  | { type: "LOGIN"; payload: User }
+  | { type: "LOGOUT"; payload: null }
+  | { type: "REFRESH"; payload: { accessToken: string; refreshToken: string } };
 
 const initalState = { user: null };
 export const AuthContext = createContext<{
-	state: AuthContextType;
-	dispatch: React.Dispatch<ACTIONTYPE>;
+  state: AuthContextType;
+  dispatch: React.Dispatch<ACTIONTYPE>;
 }>({ state: initalState, dispatch: () => null });
 
 const authReducer = (prevState: AuthContextType, action: ACTIONTYPE) => {
-	switch (action.type) {
-		case "LOGIN":
-			return { user: action.payload };
-		case "LOGOUT":
-			return { user: null };
-		default:
-			return prevState;
-	}
+  switch (action.type) {
+    case "LOGIN":
+      return { user: action.payload };
+    case "LOGOUT":
+      return { user: null };
+    case "REFRESH":
+      return {
+        user: {
+          ...prevState.user,
+          accessToken: action.payload.accessToken,
+          refreshToken: action.payload.refreshToken,
+        } as User,
+      };
+    default:
+      return prevState;
+  }
 };
 
-const AuthContextProvider = ({ children }: {children: JSX.Element[] | JSX.Element}) => {
-	const [state, dispatch] = useReducer(authReducer, { user: null });
-	useEffect(() => {
-		const user = localStorage.getItem("user");
-		if (user !== null) {
-			const parsedUser: User = JSON.parse(user);
-			dispatch({ type: "LOGIN", payload: parsedUser });
-		}
-	}, []);
-	return (
-		<AuthContext.Provider value={{ state, dispatch }}>
-			{children}
-		</AuthContext.Provider>
-	);
+const AuthContextProvider = ({
+  children,
+}: {
+  children: JSX.Element[] | JSX.Element;
+}) => {
+  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [cookie] = useCookies(["user"]);
+
+  useEffect(() => {
+    const user = cookie.user as User;
+    if (user !== null) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContextProvider;
+
