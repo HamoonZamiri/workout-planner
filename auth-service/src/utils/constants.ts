457ab18f9616b349514afdc4b8c88ffc74dccdce
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-03-06 12:18:33 -0500

    fix: replace with proper default url

--- auth-service/src/utils/constants.ts
+++ auth-service/src/utils/constants.ts
@@ -1,4 +1,4 @@
-const RABBITMQ_URL = process.env.RABBITMQ_URL || "ampq://localhost:5672";
+const RABBITMQ_URL = process.env.RABBITMQ_URL || "amqp://localhost:5672";
 
 const constants = {
   RABBITMQ_URL,
