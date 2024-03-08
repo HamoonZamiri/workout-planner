43824ff1af18233eab15191d1fa4095bbe63ca76
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-03-07 20:18:48 -0500

    feat: user_created pub sub pattern initial implementation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,10 +6,39 @@ import AppError from "./utils/AppError";
 import { FitlogCoreDataSource } from "./utils/pgres.datasource";
 import RoutineRouter from "./routine/routes/routine.routes";
 import morgan from "morgan";
+import amqp from "amqplib";
+import constants from "./utils/constants";
+import { handleUserCreated } from "./routine/services/routine.service";
 dotenv.config();
 
+export async function connectToMQ() {
+  try {
+    const connection = await amqp.connect(constants.RABBITMQ_URL);
+    const channel = await connection.createChannel();
+    return { connection, channel };
+  } catch (err) {
+    console.error(err);
+  }
+}
 export default async function initializeApp() {
   await FitlogCoreDataSource.initialize();
+  const connectionResult = await connectToMQ();
+  if (connectionResult === undefined) {
+    throw new Error("Failed to connect to RabbitMQ");
+  }
+
+  const { channel } = connectionResult;
+  await channel.assertExchange("user_created", "fanout", { durable: false });
+  const assertQueue = await channel.assertQueue("", { exclusive: true });
+  await channel.bindQueue(assertQueue.queue, "user_created", "");
+
+  channel.consume(assertQueue.queue, async (message) => {
+    if (message === null) {
+      console.log("Null message was received");
+      return;
+    }
+    await handleUserCreated(message);
+  });
   const app = express();
 
   // middleware
