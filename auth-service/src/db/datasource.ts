e2c5c64382df676141c155d538746cd0b984c5e5
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-03-12 17:04:14 -0400

    chore: access env variables using constants object

--- auth-service/src/db/datasource.ts
+++ auth-service/src/db/datasource.ts
@@ -1,15 +1,16 @@
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
+import constants from "../utils/constants";
 dotenv.config();
 
 export const AuthDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
+  host: constants.DB_HOST,
   port: 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  username: constants.DB_USERNAME,
+  password: constants.DB_PASSWORD,
   database:
-    process.env.NODE_ENV === "test" ? "test_auth_service" : process.env.DB_NAME,
+    process.env.NODE_ENV === "test" ? "test_auth_service" : constants.database,
   synchronize: true,
   entities: [__dirname + "/../**/*.entity.{js,ts}"],
 });
--- auth-service/src/utils/constants.ts
+++ auth-service/src/utils/constants.ts
@@ -1,6 +1,18 @@
-const RABBITMQ_URL = process.env.RABBITMQ_URL || "amqp://localhost:5672";
+import dotenv from "dotenv";
+dotenv.config();
+
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_USERNAME = process.env.DB_USERNAME || "postgres";
+const DB_PASSWORD = process.env.DB_PASSWORD || "postgres";
+const database =
+  process.env.NODE_ENV === "test" ? "test_auth_service" : process.env.DB_NAME;
+const RABBITMQ_URL = process.env.RABBITMQ_URL || "amqp://localhost";
 
 const constants = {
   RABBITMQ_URL,
+  DB_PASSWORD,
+  DB_USERNAME,
+  DB_HOST,
+  database,
 } as const;
 export default constants;
