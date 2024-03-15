fe9881a86ddfd33b9f2d9aa11dd849323ec10ce5
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-03-15 16:55:15 -0400

    feat: two-way communication between clients

--- chat-service/main.go
+++ chat-service/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"workout-planner/chat/clients"
 
 	"github.com/gorilla/websocket"
 )
@@ -12,33 +13,73 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+var clientMap = make(map[string]*clients.SocketClient)
+
+type Message struct {
+	FromUser string `json:"fromUser"`
+	ToUser   string `json:"toUser"`
+	Content  string `json:"content"`
+}
+
+func closeConnection(client *clients.SocketClient) {
+	client.CloseConn()
+	delete(clientMap, client.GetUserId())
+}
+
 // initial implementation borrowed from https://gowebexamples.com/websockets/
 func main() {
 	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
-			panic(err)
+			fmt.Printf("Error: %s\n", err)
 		}
+
+		userId := r.URL.Query().Get("userId")
+		if userId == "" {
+			fmt.Println("User ID not found")
+			return
+		}
+
+		fmt.Printf("User %s connected\n", userId)
+
+		socketClient := clients.New(conn, userId)
+		clientMap[userId] = socketClient
+
+		defer closeConnection(socketClient)
 
 		for {
 			// Read message from browser
-			messageType, message, err := conn.ReadMessage()
+			var parsedMessage Message
+			err := conn.ReadJSON(&parsedMessage)
 			if err != nil {
-				panic(err)
+				fmt.Println(err, "Problem reading json message")
 			}
+
+			if err != nil {
+				fmt.Println(err, "Problem parsing json message")
+			}
+
+			// if the other user is still active send the message real-time
+			if clientMap[parsedMessage.ToUser] != nil {
+				receiver := clientMap[parsedMessage.ToUser]
+				err = receiver.GetConn().WriteMessage(websocket.TextMessage, []byte(parsedMessage.Content))
+				if err != nil {
+					fmt.Println(err, "Problem sending message to receiver")
+				}
+			}
+
+			// TODO: save the message to the database
 
 			// Print the message to the console
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(message))
-
-			serverMessage := []byte(fmt.Sprintf("You sent: %s", string(message)))
-			if err = conn.WriteMessage(messageType, serverMessage); err != nil {
-				fmt.Println(err)
-			}
+			fmt.Printf("%s id=%s sent: %s to %s\n", conn.RemoteAddr(), parsedMessage.FromUser, parsedMessage.Content, parsedMessage.ToUser)
 		}
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "statics/index.html")
+		http.ServeFile(w, r, "statics/client1.html")
+	})
+	http.HandleFunc("/user2", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "statics/client2.html")
 	})
 
 	http.ListenAndServe(":8083", nil)
