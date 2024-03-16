c6ab9afe6541861a61ad3cb7ac876764372c0ac8
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-03-16 17:18:03 -0400

    chore: some refactors and renames

--- chat-service/clients/client.go
+++ chat-service/clients/client.go
@@ -1,24 +1,59 @@
 package clients
 
-import "github.com/gorilla/websocket"
+import (
+	"fmt"
+	"net/url"
+	"time"
 
-type SocketClient struct {
+	"github.com/gorilla/websocket"
+)
+
+type WebSocketClient struct {
 	conn   *websocket.Conn
 	userId string
 }
 
-func (c *SocketClient) GetConn() *websocket.Conn {
+func (c *WebSocketClient) GetConn() *websocket.Conn {
 	return c.conn
 }
 
-func (c *SocketClient) GetUserId() string {
+func (c *WebSocketClient) GetUserId() string {
 	return c.userId
 }
 
-func (c *SocketClient) CloseConn() {
+func (c *WebSocketClient) CloseConn() {
 	c.conn.Close()
 }
 
-func New(conn *websocket.Conn, userId string) *SocketClient {
-	return &SocketClient{conn, userId}
+func NewSocketClient(conn *websocket.Conn, userId string) *WebSocketClient {
+	return &WebSocketClient{conn, userId}
+}
+
+func Connect(userId string) (*WebSocketClient, error) {
+	u := url.URL{Scheme: "ws", Host: "localhost:8083", Path: "/echo"}
+	query := u.Query()
+	query.Set("userId", userId)
+	u.RawQuery = query.Encode()
+
+	conn, res, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		fmt.Println(res.Request.URL)
+		return nil, err
+	}
+
+	return &WebSocketClient{conn, userId}, nil
+}
+
+func (c *WebSocketClient) SendMessage(toUser string, content string) error {
+	message := (map[string]string{"fromUser": c.userId, "toUser": toUser, "content": content})
+	return c.conn.WriteJSON(message)
+}
+
+func (c *WebSocketClient) ReceiveMessage() (string, error) {
+	c.conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+	_, message, err := c.conn.ReadMessage()
+	if err != nil {
+		return "", err
+	}
+	return string(message), nil
 }
--- chat-service/main.go
+++ chat-service/main.go
@@ -13,7 +13,7 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-var clientMap = make(map[string]*clients.SocketClient)
+var clientMap = make(map[string]*clients.WebSocketClient)
 
 type Message struct {
 	FromUser string `json:"fromUser"`
@@ -21,59 +21,58 @@ type Message struct {
 	Content  string `json:"content"`
 }
 
-func closeConnection(client *clients.SocketClient) {
+func closeConnection(client *clients.WebSocketClient) {
 	client.CloseConn()
 	delete(clientMap, client.GetUserId())
 }
 
-// initial implementation borrowed from https://gowebexamples.com/websockets/
-func main() {
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			fmt.Printf("Error: %s\n", err)
-		}
+func handleSocketConn(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
 
-		userId := r.URL.Query().Get("userId")
-		if userId == "" {
-			fmt.Println("User ID not found")
+	userId := r.URL.Query().Get("userId")
+	if userId == "" {
+		fmt.Println("User ID not found")
+		return
+	}
+
+	fmt.Printf("User %s connected\n", userId)
+
+	socketClient := clients.NewSocketClient(conn, userId)
+	clientMap[userId] = socketClient
+
+	defer closeConnection(socketClient)
+
+	for {
+		// Read message from browser
+		var parsedMessage Message
+		err := conn.ReadJSON(&parsedMessage)
+		if err != nil {
+			fmt.Println(err, "Problem reading json message")
 			return
 		}
 
-		fmt.Printf("User %s connected\n", userId)
-
-		socketClient := clients.New(conn, userId)
-		clientMap[userId] = socketClient
-
-		defer closeConnection(socketClient)
-
-		for {
-			// Read message from browser
-			var parsedMessage Message
-			err := conn.ReadJSON(&parsedMessage)
+		// if the other user is still active send the message real-time
+		if clientMap[parsedMessage.ToUser] != nil {
+			receiver := clientMap[parsedMessage.ToUser]
+			err = receiver.GetConn().WriteMessage(websocket.TextMessage, []byte(parsedMessage.Content))
 			if err != nil {
-				fmt.Println(err, "Problem reading json message")
+				fmt.Println(err, "Problem sending message to receiver")
+				return
 			}
-
-			if err != nil {
-				fmt.Println(err, "Problem parsing json message")
-			}
-
-			// if the other user is still active send the message real-time
-			if clientMap[parsedMessage.ToUser] != nil {
-				receiver := clientMap[parsedMessage.ToUser]
-				err = receiver.GetConn().WriteMessage(websocket.TextMessage, []byte(parsedMessage.Content))
-				if err != nil {
-					fmt.Println(err, "Problem sending message to receiver")
-				}
-			}
-
-			// TODO: save the message to the database
-
-			// Print the message to the console
-			fmt.Printf("%s id=%s sent: %s to %s\n", conn.RemoteAddr(), parsedMessage.FromUser, parsedMessage.Content, parsedMessage.ToUser)
 		}
-	})
+
+		// TODO: save the message to the database
+
+		// Print the message to the console
+		fmt.Printf("%s id=%s sent: %s to %s\n", conn.RemoteAddr(), parsedMessage.FromUser, parsedMessage.Content, parsedMessage.ToUser)
+	}
+}
+
+func startServer() {
+	http.HandleFunc("/echo", handleSocketConn)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "statics/client1.html")
@@ -83,4 +82,9 @@ func main() {
 	})
 
 	http.ListenAndServe(":8083", nil)
+}
+
+// initial implementation borrowed from https://gowebexamples.com/websockets/
+func main() {
+	startServer()
 }
