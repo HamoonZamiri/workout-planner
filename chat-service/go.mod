b29f593151220b83f4e146efebba01ede3c73fa4
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-03-17 12:32:06 -0400

    feat: add client connect and disconnect tests, add assertion library

--- chat-service/go.mod
+++ chat-service/go.mod
@@ -4,4 +4,10 @@ go 1.22.1
 
 require github.com/gorilla/websocket v1.5.1
 
-require golang.org/x/net v0.17.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- chat-service/tests/main_test.go
+++ chat-service/tests/main_test.go
@@ -2,37 +2,48 @@ package main
 
 import (
 	"testing"
+	"workout-planner/chat/api/handler"
 	"workout-planner/chat/clients"
 	"workout-planner/chat/config"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestTwoClients(t *testing.T) {
-	go config.StartServer()
+var h *handler.SocketHandler
 
+func init() {
+	h = handler.New()
+	go config.StartServer(h)
+}
+
+func TestClientConnect(t *testing.T) {
+	client, err := clients.Connect("1")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+}
+
+func TestClientDisconnect(t *testing.T) {
+	client, err := clients.Connect("1")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	h.CloseConnection(client, "1")
+
+	client = h.GetClient("1")
+	assert.Nil(t, client)
+}
+
+func TestTwoClients(t *testing.T) {
 	client1, err := clients.Connect("1")
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 
 	client2, err := clients.Connect("2")
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 
 	err = client1.SendMessage("2", "Hello")
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 
 	message, err := client2.ReceiveMessage()
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 
-	if message != "Hello" {
-		t.Error("Message not received")
-	}
-
-	client1.CloseConn()
-	client2.CloseConn()
+	assert.Equal(t, "Hello", message, "Message should be 'Hello'")
 }
