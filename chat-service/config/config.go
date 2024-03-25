13a56969f27ece508e4dd284e4f9623ef154f401
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-03-25 14:32:34 -0400

    fix: replace basic ids with uuids

--- chat-service/config/config.go
+++ chat-service/config/config.go
@@ -2,17 +2,22 @@ package config
 
 import (
 	"net/http"
+	"path/filepath"
+	"runtime"
 	"workout-planner/chat/api/handler"
 )
 
 func StartServer(handler *handler.SocketHandler) {
+	_, currentFile, _, _ := runtime.Caller(0)
+	staticDir := filepath.Join(filepath.Dir(currentFile), "../static")
+
 	http.HandleFunc("/echo", handler.HandleSocketConn)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../static/client1.html")
+		http.ServeFile(w, r, filepath.Join(staticDir, "client1.html"))
 	})
 	http.HandleFunc("/user2", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../static/client2.html")
+		http.ServeFile(w, r, filepath.Join(staticDir, "client2.html"))
 	})
 
 	http.ListenAndServe(":8083", nil)
